9c0996b2887ba32ae7e22d5f7376c77a1663b612
Ivanq <imachug@yandex.ru>
2019-05-22 14:07:03 +0300

    Verify that Ark crypto library is installed

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
+try:
+	import crypto
+except ImportError:
+	print("Please install Ark crypto library by running 'pip install arkecosystem-crypto'")
+	raise SystemExit(1)
+
+
 from Crypt import Cryptography
 from . import CryptArk
 
